b2ed55454f879ed39da31978b5ea8071ab14ccec
MahmoudMetwalli <mammkbih@gmail.com>
2024-08-15 23:51:11 +0300

    2024-08-15 11:51:11 PM

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+def index_range(page, page_size):
+    """index helper function"""
+    return (((page - 1) * page_size),(page * page_size))
